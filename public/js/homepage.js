0af7f66c57162f18157c4db754542db1d46f3cf8
Carter <carreejoh4637@gmail.com>
2023-06-19 18:58:10 -0600

    Need to take a break, public js files arent being reached, cant find a way to target individual posts

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,27 +1,30 @@
-const logoutSubmit = document.querySelector('#logoutBtn');
-const submitComment = document.getElementById('submitCommentBtn');
-const commentContent = document.getElementById('newComment');
+// // const logoutSubmit = document.querySelector('#logoutBtn');
+// const submitComment = document.querySelector('.submitCommentBtn');
+// const commentContent = document.getElementById('newComment');
+
+// let commentBtn = document.getElementById('.commentBtn');
+
+// // const commitLogout = async () => {
+// //     const response = await fetch('/api/user/logout', {
+// //         method: 'POST'
+// //     });
+
+// //     if(response.ok) {
+// //         window.location.replace('/');
+// //     } else {
+// //         alert('Cannot logout');
+// //     }
+// // }
+
+// // const addComment = async () => {
+// //     let postId = submitComment.id;
+// //     console.log(postId);
+// //     console.log("hello")
+// // }
+// function addComment() {
+//     console.log("hello")
+// }
 
 
-
-const commitLogout = async () => {
-    const response = await fetch('/api/user/logout', {
-        method: 'POST'
-    });
-
-    if(response.ok) {
-        window.location.replace('/');
-    } else {
-        alert('Cannot logout');
-    }
-}
-
-
-
-
-
-
-
-
-
-logoutSubmit.addEventListener("click", commitLogout);
+// submitComment.addEventListener("click", addComment);
+// // logoutSubmit.addEventListener("click", commitLogout);
